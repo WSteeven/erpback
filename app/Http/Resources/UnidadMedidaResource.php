89076f0f4b4d00bbaed5e04298865c61ccbc2a62
Steeven Cordova <wilson972906@gmail.com>
2023-02-06 22:31:36 -0500

    correcciones unidades de medida

--- app/Http/Resources/UnidadMedidaResource.php
+++ app/Http/Resources/UnidadMedidaResource.php
@@ -14,6 +14,11 @@ class UnidadMedidaResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'id'=>$this->id,
+            'nombre'=>$this->nombre,
+            'simbolo'=>$this->simbolo,
+        ];
     }
 }
