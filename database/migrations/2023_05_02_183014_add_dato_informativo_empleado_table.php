4427e7d1d60e6c7866df4342088eaae869d31645
henvisi-1994 <henvisi_1994@hotmail.com>
2023-05-16 18:02:12 -0500

    orden en tabla de empleado y doble a decimal en rol de pagos

--- database/migrations/2023_05_02_183014_add_dato_informativo_empleado_table.php
+++ database/migrations/2023_05_02_183014_add_dato_informativo_empleado_table.php
@@ -14,19 +14,19 @@ return new class extends Migration
     public function up()
     {
         Schema::table('empleados', function (Blueprint $table) {
-            $table->string('tipo_sangre');
-            $table->text('direccion');
-            $table->double('salario');
-            $table->string('num_cuenta_bancaria');
-            $table->boolean('tiene_discapacidad');
-            $table->string('fecha_ingreso')->nullable();
-            $table->string('fecha_salida')->nullable();
-            $table->text('observacion');
-            $table->unsignedBigInteger('estado_civil_id')->nullable();
+            $table->string('tipo_sangre')->after('responsable_discapacitados');
+            $table->text('direccion')->after('tipo_sangre');
+            $table->decimal('salario',8,2)->after('direccion');
+            $table->string('num_cuenta_bancaria')->after('salario');
+            $table->boolean('tiene_discapacidad')->after('num_cuenta_bancaria');
+            $table->string('fecha_ingreso')->after('tiene_discapacidad')->nullable();
+            $table->string('fecha_salida')->after('fecha_ingreso')->nullable();
+            $table->text('observacion')->after('fecha_salida');
+            $table->unsignedBigInteger('estado_civil_id')->after('observacion')->nullable();
             $table->foreign('estado_civil_id')->references('id')->on('estado_civil');
-            $table->unsignedBigInteger('area_id')->nullable();
+            $table->unsignedBigInteger('area_id')->after('estado_civil_id')->nullable();
             $table->foreign('area_id')->references('id')->on('areas');
-            $table->unsignedBigInteger('tipo_contrato_id')->nullable();
+            $table->unsignedBigInteger('tipo_contrato_id')->after('area_id')->nullable();
             $table->foreign('tipo_contrato_id')->references('id')->on('tipo_contrato');
         });
     }
