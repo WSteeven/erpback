8211f9cc09e1da497a6f52680f2a1bff5d37e9d9
henvisi-1994 <henvisi_1994@hotmail.com>
2023-05-23 11:13:19 -0500

    correcciones de diseño de reporte de acreditaciones

--- resources/views/exports/reportes/reporte_consolidado/reporte_acreditaciones_usuario.blade.php
+++ resources/views/exports/reportes/reporte_consolidado/reporte_acreditaciones_usuario.blade.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Reporte Acreditaciones</title>
+    <title>Reporte de Acreditaciones</title>
     <style>
         @page {
             margin: 2px 15px 5px 15px;
         }
 
         body {
-            /* background-image: url({{ 'data:image/png;base64,'. base64_encode(file_get_contents('img/logoJPBN_10.png')) }}); */
-            background-image: url({{ 'data:image/png;base64,'. base64_encode(file_get_contents('img/logoJPBN_10.png')) }});
+            /* background-image: url({{ 'data:image/png;base64,' . base64_encode(file_get_contents('img/logoJPBN_10.png')) }}); */
+            background-image: url({{ 'data:image/png;base64,' . base64_encode(file_get_contents('img/logoJPBN_10.png')) }});
             background-repeat: no-repeat;
             background-position: center;
         }
@@ -33,7 +33,7 @@
         /** Definir las reglas del pie de página **/
         footer {
             position: fixed;
-            bottom: 10px;
+            bottom: 5px;
             left: 0cm;
             right: 0cm;
             height: 2cm;
@@ -53,7 +53,7 @@
             top: 80px;
             left: 0cm;
             right: 0cm;
-            margin-bottom: 7cm;
+            margin-bottom: 4.3cm;
             font-size: 12px;
         }
 
@@ -64,15 +64,6 @@
         h1 {
             text-align: center;
             text-transform: uppercase;
-        }
-
-        .firma {
-            table-layout: fixed;
-            width: 100%;
-            line-height: normal;
-            font-size: 12px;
-            /* position: inherit; */
-            /* top: 140px; */
         }
 
 
@@ -92,11 +83,14 @@
             style="color:#000000; table-layout:fixed; width: 100%; font-family:Verdana, Arial, Helvetica, sans-serif; font-size:18px;">
             <tr class="row" style="width:auto">
                 <td style="width: 10%;">
-                    <div class="col-md-3"><img src="{{ 'data:image/png;base64,'. base64_encode(file_get_contents('img/logoJP.png')) }}" width="90"></div>
+                    <div class="col-md-3"><img
+                            src="{{ 'data:image/png;base64,' . base64_encode(file_get_contents('img/logoJP.png')) }}"
+                            width="90"></div>
                 </td>
                 <td style="width: 100%">
                     <div class="col-md-7" align="center"><b style="font-size: 75%">REPORTE DE ACREDITACIONES
-                            {{' DEL ' . date("d-m-Y", strtotime( $fecha_inicio)) . ' AL ' .date("d-m-Y", strtotime( $fecha_fin)) }}</b></div>
+                            {{ ' DEL ' . date('d-m-Y', strtotime($fecha_inicio)) . ' AL ' . date('d-m-Y', strtotime($fecha_fin)) }}</b>
+                    </div>
                 </td>
             </tr>
         </table>
@@ -105,9 +99,9 @@
     <footer>
         <table style="width: 100%;">
             <tr>
-                <td class="page">Página </td>
                 <td style="line-height: normal;">
-                    <div style="margin: 0%; margin-bottom: 0px; margin-top: 0px;" align="center">Esta informacion es propiedad de  JPCONSTRUCRED C.LTDA. - Prohibida su divulgacion
+                    <div style="margin: 0%; margin-bottom: 0px; margin-top: 0px;" align="center">Esta informacion es
+                        propiedad de JPCONSTRUCRED C.LTDA. - Prohibida su divulgacion
                     </div>
                     <div style="margin: 0%; margin-bottom: 0px; margin-top: 0px;" align="center">Generado por el
                         Usuario:
@@ -120,69 +114,57 @@
         </table>
     </footer>
     <main>
-        <table
-            style="color:#000000; table-layout:fixed; width: 100%; font-family:Verdana, Arial, Helvetica, sans-serif; font-size:10px;margin-top: 20px;">
-            <tr height="29">
-                <td height="15">
-                    <div align="center">
-                        <table width="100%">
-                            <tr>
-                                @if(isset($usuario->nombres))
-                                <td bgcolor="#bfbfbf" style="font-size:12px">
-                                    <div align="center"><strong>{{ $usuario->nombres.' '.$usuario->apellidos }} </strong></div>
-                                </td>
-                                @endif
-                            </tr>
-                            <tr>
-                                <td height="55px;">
-                                    <table width="100%" border="1" align="left" cellpadding="0" cellspacing="0">
-                                        <tr>
-                                            <td bgcolor="#a9d08e" style="font-size:10px" width="29%">
-                                                <div align="center"><strong>Nombres y Apellidos</strong></div>
-                                            </td>
-                                            <td bgcolor="#a9d08e" style="font-size:10px" width="15%">
-                                                <div align="center"><strong>Usuario</strong></div>
-                                            </td>
-                                            <td bgcolor="#a9d08e" style="font-size:10px" width="17%">
-                                                <div align="center"><strong>Fecha</strong></div>
-                                            </td>
-                                            <td bgcolor="#a9d08e" style="font-size:10px" width="29%">
-                                                <div align="center"><strong>Descripci&oacute;n</strong></div>
-                                            </td>
-                                            <td bgcolor="#a9d08e" style="font-size:10px" width="10%">
-                                                <div align="center"><strong>Monto</strong></div>
-                                            </td>
-                                        </tr>
-                                        @foreach ($acreditaciones as $acreditacion)
-                                            <tr>
-                                                <td style="font-size:10px" width="29%">
-                                                    <div align="left">
-                                                        {{ $acreditacion['empleado']->nombres . ' ' . $acreditacion['empleado']->apellidos }}
-                                                    </div>
-                                                </td>
-                                                <td style="font-size:10px" width="15%">
-                                                    <div align="left">{{ $acreditacion['usuario']->name }}
-                                                    </div>
-                                                </td>
-                                                <td style="font-size:10px" width="17%">
-                                                    <div align="center">{{ date("d-m-Y", strtotime(  $acreditacion['fecha'])) }}</div>
-                                                </td>
-                                                <td style="font-size:10px" width="29%">
-                                                    <div align="left">{{ $acreditacion['descripcion_acreditacion'] }}</div>
-                                                </td>
-                                                <td style="font-size:10px" width="10%">
-                                                    <div align="right">
-                                                        {{ number_format($acreditacion['monto'], 2, ',', '.') }}</div>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </table>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
+        @if (isset($usuario->nombres))
+            <p
+                style="color:#000000; table-layout:fixed; width: 100%; font-family:Verdana, Arial, Helvetica, sans-serif; font-size:12; font-weight:bold; margin-top: -6px;">
+            <div align="center"><strong>{{ $usuario->nombres . ' ' . $usuario->apellidos }}
+                </strong></div>
+            </p>
+            <br>
+        @endif
+        <table width="100%" border="1" align="left" cellpadding="0" cellspacing="0">
+            <tr>
+                <td bgcolor="#a9d08e" style="font-size:10px" width="29%">
+                    <div align="center"><strong>Nombres y Apellidos</strong></div>
+                </td>
+                <td bgcolor="#a9d08e" style="font-size:10px" width="15%">
+                    <div align="center"><strong>Usuario</strong></div>
+                </td>
+                <td bgcolor="#a9d08e" style="font-size:10px" width="17%">
+                    <div align="center"><strong>Fecha</strong></div>
+                </td>
+                <td bgcolor="#a9d08e" style="font-size:10px" width="29%">
+                    <div align="center"><strong>Descripci&oacute;n</strong></div>
+                </td>
+                <td bgcolor="#a9d08e" style="font-size:10px" width="10%">
+                    <div align="center"><strong>Monto</strong></div>
                 </td>
             </tr>
+            @foreach ($acreditaciones as $acreditacion)
+                <tr>
+                    <td style="font-size:10px" width="29%">
+                        <div align="left">
+                            {{ $acreditacion['empleado']->nombres . ' ' . $acreditacion['empleado']->apellidos }}
+                        </div>
+                    </td>
+                    <td style="font-size:10px" width="15%">
+                        <div align="left">{{ $acreditacion['usuario']->name }}
+                        </div>
+                    </td>
+                    <td style="font-size:10px" width="17%">
+                        <div align="center">
+                            {{ date('d-m-Y', strtotime($acreditacion['fecha'])) }}</div>
+                    </td>
+                    <td style="font-size:10px" width="29%">
+                        <div align="left">{{ $acreditacion['descripcion_acreditacion'] }}
+                        </div>
+                    </td>
+                    <td style="font-size:10px" width="10%">
+                        <div align="right">
+                            {{ number_format($acreditacion['monto'], 2, ',', '.') }}</div>
+                    </td>
+                </tr>
+            @endforeach
         </table>
     </main>
 </body>
