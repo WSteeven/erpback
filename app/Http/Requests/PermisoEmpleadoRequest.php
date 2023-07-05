7da79849f4133e7b5bfd92754e7f1323643193fc
Henry  Vinicio Simbaña Cruz <henvisi_1994@hotmail.com>
2023-07-05 10:37:52 -0500

    cambios en request de permiso a empleados

--- app/Http/Requests/PermisoEmpleadoRequest.php
+++ app/Http/Requests/PermisoEmpleadoRequest.php
@@ -32,6 +32,8 @@ class PermisoEmpleadoRequest extends FormRequest
             'fecha_recuperacion' => 'nullable|date_format:Y-m-d',
             'hora_recuperacion' => 'nullable|string',
             'justificacion' => 'required|string',
+            'observacion'=> 'nullable|string'
+            'fecha_hora_reagendamiento'=> 'nullable|string',
             'empleado_id' => 'required|exists:empleados,id',
             'tieneDocumento' => 'required'
         ];
@@ -40,6 +42,9 @@ class PermisoEmpleadoRequest extends FormRequest
     {
         $fecha_inicio = Carbon::createFromFormat('d-m-Y H:i', $this->fecha_hora_inicio);
         $fecha_fin = Carbon::createFromFormat('d-m-Y H:i', $this->fecha_hora_fin);
+       if ($this->fecha_hora_reagendamiento != null) {
+        $fecha_hora_reagendamiento = Carbon::createFromForrmat('d-m-Y H:i',$this->fecha_hora_reagendamiento);
+       }
         if($this->fecha_recuperacion !=null) {
         $fecha_recuperacion = Carbon::createFromFormat('d-m-Y',$this->fecha_recuperacion);
         }
@@ -51,6 +56,12 @@ class PermisoEmpleadoRequest extends FormRequest
             'fecha_hora_inicio' =>  $fecha_inicio->format('Y-m-d H:i:s'),
             'fecha_hora_fin' =>  $fecha_fin->format('Y-m-d H:i:s'),
         ]);
+        if($this->fecha_hora_reagendamiento != null )
+        {
+            $this->merge([
+             'fecha_hora_reagendamiento' => $fecha_hora_reagendamiento->format('Y-m-d H:i:s');,
+            ])
+        }
         if($this->fecha_recuperacion !=null) {
             $this->merge([
                 'fecha_recuperacion' => $fecha_recuperacion->format('Y-m-d'),
