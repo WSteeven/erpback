aa285c594d18bec0486c42cd3f7d3e12deb08730
juancuestav <juanbryanc@gmail.com>
2023-09-05 23:54:04 -0500

    Tickets: Dashboard

--- app/Http/Controllers/DashboardTareaController.php
+++ app/Http/Controllers/DashboardTareaController.php
@@ -3,14 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\SubtareaResource;
+use Src\App\DashboardTareaService;
 use App\Models\Empleado;
 use App\Models\Subtarea;
-use App\Models\Ticket;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
-use Src\App\DashboardTareaService;
 
 class DashboardTareaController extends Controller
 {
@@ -34,7 +29,6 @@ class DashboardTareaController extends Controller
 
         $subtareasCoordinador = $this->dashboardTareaService->obtenerSubtareasFechaInicioFin($coordinador, $campos);
         $subtareasFechaInicioFin = $subtareasCoordinador->pluck('estado');
-        // Log::channel('testing')->info('Log', compact('subtareasCoordinador'));
 
         $cantidadSubtareasAgendadas = $this->dashboardTareaService->contarCantidadSubtareasPorEstado($subtareasFechaInicioFin, Subtarea::AGENDADO);
         $cantidadSubtareasEjecutadas = $this->dashboardTareaService->contarCantidadSubtareasPorEstado($subtareasFechaInicioFin, Subtarea::EJECUTANDO);
@@ -84,32 +78,4 @@ class DashboardTareaController extends Controller
 
         return response()->json(compact('results'));
     }
-
-    /* BORRRAR - private function obtenerCantidadSubtareasPorGrupos($idCoordinador)
-    {
-        $fechaInicio = request('fecha_inicio');
-        $fechaFin = request('fecha_fin');
-
-        // Conversion de fechas
-        $fechaInicio = Carbon::createFromFormat('d-m-Y', $fechaInicio)->format('Y-m-d');
-        $fechaFin = Carbon::createFromFormat('d-m-Y', $fechaFin)->addDay()->toDateString();
-
-        return DB::table('subtareas')->select('grupos.nombre', DB::raw('COUNT(subtareas.grupo_id) as total_subtareas'), 'subtareas.estado')
-            ->join('tareas', 'subtareas.tarea_id', '=', 'tareas.id')
-            ->join('grupos', 'subtareas.grupo_id', '=', 'grupos.id')
-            ->where('tareas.coordinador_id', $idCoordinador)
-            ->whereBetween('tareas.created_at', [$fechaInicio, $fechaFin])
-            ->groupBy('subtareas.grupo_id')
-            ->groupBy('subtareas.estado')
-            ->get();
-    } */
 }
-
-/**
- select grupos.nombre, subtareas.estado, count(subtareas.grupo_id) as cantidad_subtareas from subtareas
-inner join tareas on subtareas.tarea_id = tareas.id
-inner join grupos on subtareas.grupo_id = grupos.id
-where tareas.coordinador_id = 7
-group by subtareas.grupo_id;
-
- */
