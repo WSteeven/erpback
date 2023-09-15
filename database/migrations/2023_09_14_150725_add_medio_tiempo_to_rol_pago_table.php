a706e6549e9ebfe4be0326c28e4d2fe4915262b5
henvisi-1994 <henvisi_1994@hotmail.com>
2023-09-15 18:03:18 -0500

    Cambios en rol de pagos

--- database/migrations/2023_09_14_150725_add_medio_tiempo_to_rol_pago_table.php
+++ database/migrations/2023_09_14_150725_add_medio_tiempo_to_rol_pago_table.php
@@ -14,7 +14,25 @@ return new class extends Migration
     public function up()
     {
         Schema::table('rol_pago', function (Blueprint $table) {
-            $table->boolean('medio_tiempo')->default('0')->nullable();
+            $table->boolean('medio_tiempo')->default('0')->after('estado')->nullable();
+            $table->decimal('salario',8,4)->change();
+            $table->decimal('sueldo',8,4)->change();
+            $table->decimal('decimo_tercero',8,4)->change();
+            $table->decimal('decimo_cuarto',8,4)->change();
+            $table->decimal('fondos_reserva',8,4)->change();
+            $table->decimal('bonificacion',8,4)->change();
+            $table->decimal('total_ingreso',8,4)->change();
+            $table->decimal('comisiones',8,4)->change();
+            $table->decimal('iess',8,4)->change();
+            $table->decimal('anticipo',8,4)->change();
+            $table->decimal('prestamo_quirorafario',8,4)->change();
+            $table->decimal('prestamo_hipotecario',8,4)->change();
+            $table->decimal('extension_conyugal',8,4)->change();
+            $table->decimal('prestamo_empresarial',8,4)->change();
+            $table->decimal('bono_recurente',8,4)->change();
+            $table->decimal('total_egreso',8,4)->change();
+            $table->decimal('total',8.4)->change();;
+
         });
     }
 
