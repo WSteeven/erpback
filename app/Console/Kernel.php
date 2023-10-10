c677ae236fcc1bc2c4622275c3fc8ad657db92b6
henvisi-1994 <henvisi_1994@hotmail.com>
2023-10-10 17:35:05 -0500

    fix: antiguedad del empleado

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use App\Jobs\AnularProformaJob;
 use App\Jobs\MyJobExample;
+use App\Jobs\NotificarVacacionesJob;
 use App\Jobs\RechazarGastoJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -22,6 +23,7 @@ class Kernel extends ConsoleKernel
         // $schedule->job(new MyJobExample)->everyMinute(); // Execute job every 5 minutes
         $schedule->job(new AnularProformaJob)->dailyAt('08:00'); // Execute job every day at 08:00
         $schedule->job(new RechazarGastoJob)->monthly();
+        $schedule->job(new NotificarVacacionesJob)->everyMinute();
         // $schedule->job(new MyJobExample)->dailyAt('08:00'); // Execute job every
 
         // $colocar el job que envia el comprobante a recursos humanos y sso cuando ya finalice
