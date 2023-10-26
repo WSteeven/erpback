728746958ddcd5e0e17b85dae91e3312e015cd8a
henvisi-1994 <henvisi_1994@hotmail.com>
2023-10-26 14:20:17 -0500

    feat:listado de bonos

---
+++ database/seeders/BonosSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Ventas\Bonos;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BonosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Bonos::insert([
+            ['cant_ventas' => 6,'valor'=>20],
+            ['cant_ventas' => 12,'valor'=>50],
+            ['cant_ventas' => 16,'valor'=>75],
+
+        ]);
+    }
+}
