8ccaaf8960975db0824b1d46832b153126b9fd9a
WSteeven <wilson972906@gmail.com>
2023-10-31 18:10:57 -0500

    feat: nuevo campo en tabla productos para poder filtrar las ordenes de compras segun el tipo:bien o servicio

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -4,11 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ProductoRequest;
 use App\Http\Resources\ProductoResource;
-use App\Http\Resources\TipoTareaResource;
 use App\Models\Producto;
-use App\Models\TipoTrabajo;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Src\Shared\Utils;
 
 class ProductoController extends Controller
@@ -26,35 +23,34 @@ class ProductoController extends Controller
      */
     public function index(Request $request)
     {
-        Log::channel('testing')->info('Log', ['Request recibida en productos', $request->all()]);
-        $page = $request['page'];
-        $campos = explode(',', $request['campos']);
         $results = [];
-        if ($request['campos']) {
-            $results = Producto::all($campos);
-            $results =ProductoResource::collection($results);
-            return response()->json(compact('results'));
-        } else if($request->categoria_id && is_null($request->categoria_id[0])){
-            if($request['search']){
-                $results = Producto::search($request['search'])->get();
-                Log::channel('testing')->info('Log', ['entro en searcgh', $results]);
-            }else{
-                $results = Producto::ignoreRequest(['campos', 'categoria_id'])->filter()->get();
-                Log::channel('testing')->info('Log', ['entro en if 42', $results]);
+        if($request->boolean('filtrarTipo')){
+            if(auth()->user()->hasPermissionTo('puede.ver.productos_servicios')){
+                if ($request->search) {
+                    $results = Producto::search($request->search)->where('tipo', Producto::SERVICIO)->get();
+                } else {
+                    $results = Producto::ignoreRequest(['campos', 'filtrarTipo'])->where('tipo', Producto::SERVICIO)->filter()->get();
+                }
+            }
+            if(auth()->user()->hasAllPermissions(['puede.ver.productos_bienes', 'puede.ver.productos_servicios'])){
+                if ($request->search) {
+                    $results = Producto::search($request->search)->get();
+                } else {
+                    $results = Producto::ignoreRequest(['campos', 'filtrarTipo'])->filter()->get();
+                }
             }
         }else{
-            if($request->search){
-                // $results = Producto::ignoreRequest(['campos','search'])->filter()->search($request['search'])->get();
-                $results = Producto::search($request->search)
-                ->when($request->categoria_id, function($query) use ($request){
+            if ($request->search) {
+                $results = Producto::search($request->search)->when($request->categoria_id, function ($query) use ($request) {
                     return $query->whereIn('categoria_id', $request->categoria_id);
                 })->get();
-                Log::channel('testing')->info('Log', ['entro en else 52', $results]);
-            }else{
-                $results = Producto::ignoreRequest(['campos'])->filter()->get();
-                Log::channel('testing')->info('Log', ['entro en else 50', $results]);
+            } else {
+                $results = Producto::ignoreRequest(['campos', 'filtrarTipo'])
+                ->when($request->hasfiltrarTipo)
+                ->filter()->get();
             }
         }
+
         $results = ProductoResource::collection($results);
         return response()->json(compact('results'));
     }
