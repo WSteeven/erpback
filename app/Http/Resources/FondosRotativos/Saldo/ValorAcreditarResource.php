3c4c4adc3d9641594d6866e10aaf7ccb95cb699d
henvisi-1994 <henvisi_1994@hotmail.com>
2023-11-13 12:13:53 -0500

    fix:valores a acreditar

--- app/Http/Resources/FondosRotativos/Saldo/ValorAcreditarResource.php
+++ app/Http/Resources/FondosRotativos/Saldo/ValorAcreditarResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\FondosRotativos\Saldo;
 
+use App\Models\FondosRotativos\Saldo\SaldoGrupo;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ValorAcreditarResource extends JsonResource
@@ -19,6 +20,7 @@ class ValorAcreditarResource extends JsonResource
             'id'=>$this->id,
             'empleado_info'=>$this->empleado->nombres!=null ?$this->empleado->nombres.' '.$this->empleado->apellidos:'',
             'empleado'=>$this->empleado_id,
+            'saldo_empleado'=>$this->obtener_saldo($this->empleado_id),
             'monto_generado'=>number_format($this->monto_generado, 2),
             'monto_modificado'=>number_format($this->monto_modificado, 2),
             'acreditacion_semana'=>$this->acreditacion_semana_id,
@@ -26,5 +28,10 @@ class ValorAcreditarResource extends JsonResource
             'acreditacion_semana_info'=>$this->acreditacion_semanal!=null?$this->acreditacion_semanal->semana:'',
         ];
         return $modelo;
+    }
+    public function obtener_saldo($empleado_id){
+        $saldo_actual = SaldoGrupo::where('id_usuario', $empleado_id)->orderBy('id', 'desc')->first();
+        $saldo_actual = $saldo_actual != null ? $saldo_actual->saldo_actual : 0;
+        return $saldo_actual;
     }
 }
