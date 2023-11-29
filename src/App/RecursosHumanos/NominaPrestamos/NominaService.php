dc98d73e64bf82eb674cff71759625699639a28c
henvisi-1994 <henvisi_1994@hotmail.com>
2023-11-29 08:50:34 -0500

    fix: :bug: Motivo gasto y calculo de dias rol de pagos
    Actualizar  , visualizar motivos  y calculo de dias tomando en cuenta 1ro de mes actual

--- src/App/RecursosHumanos/NominaPrestamos/NominaService.php
+++ src/App/RecursosHumanos/NominaPrestamos/NominaService.php
@@ -138,14 +138,13 @@ class NominaService
         if ($fechaIngresada->isCurrentMonth()) {
             // Verifica si la fecha ingresada es anterior al día 15 del mes actual
             // 18 de mes, fecha actual <15
-            if ($fechaIngresada->day < $cantidad_dias && $fechaIngresada->day > 1) {
+            if ($fechaIngresada->day < $cantidad_dias) {
                 // Resta la fecha ingresada de la fecha del 15 del mes actual
-                // Log::channel('testing')->info('Log', ['calculo de dias restantes',  $cantidad_dias, ($cantidad_dias - $fechaIngresada->day), $fechaIngresada->day]);
                 $diasRestantes = $cantidad_dias - $fechaIngresada->day + 1;
             } else {
                 // La fecha ingresada ya es igual o posterior al 15 del mes actual
                 $diasRestantes = $cantidad_dias; // No quedan días hasta el 15 del mes actual
-               // throw new Exception('No se puede calcular días sobre una fecha de ingreso posterior a la fecha actual');
+                throw new Exception('No se puede calcular días sobre una fecha de ingreso posterior a la fecha actual');
             }
         } else {
             // La fecha ingresada no pertenece al mes actual
@@ -241,7 +240,6 @@ class NominaService
     }
     public function enviar_rol_pago($rolPagoId, $destinatario)
     {
-        $nombre_reporte = 'rol_pagos';
         $roles_pagos = RolPago::where('id', $rolPagoId)->get();
         $results = RolPago::empaquetarListado($roles_pagos);
         $recursosHumanos = Departamento::where('id', 7)->first()->responsable_id;
