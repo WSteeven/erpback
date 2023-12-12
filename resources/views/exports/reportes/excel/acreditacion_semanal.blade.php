a927a61bca4b7b09ac804dfc04c5fd41679aa0cf
henvisi-1994 <henvisi_1994@hotmail.com>
2023-12-11 23:45:16 -0500

    fix: :art: Reporte de saldo semanal
    se agrego saldo actual al reporte semanal

--- resources/views/exports/reportes/excel/acreditacion_semanal.blade.php
+++ resources/views/exports/reportes/excel/acreditacion_semanal.blade.php
@@ -68,70 +68,63 @@
         <tr>
             <td>
                 <div align="center">
-                    <table width="100%">
+                    <table width="100%" border="1" cellspacing="0" bordercolor="#666666" class="gastos">
                         <tr>
-                            <td>
-                                <table width="100%" border="1" cellspacing="0" bordercolor="#666666" class="gastos">
-                                    <tr>
-                                        <td width="8%" bgcolor="#a9d08e">
-                                            <div align="center"><strong>ITEM</strong></div>
-                                        </td>
-                                        <td width="50%" bgcolor="#a9d08e">
-                                            <div align="center"><strong>EMPLEADO</strong></div>
-                                        </td>
-                                        <td width="15%" bgcolor="#a9d08e">
-                                            <div align="center"><strong>MONTO GENERADO</strong></div>
-                                        </td>
-                                        <td width="17%" bgcolor="#a9d08e">
-                                            <div align="center"><strong>MONTO MODIFICADO</strong></div>
-                                        </td>
-                                        <td width="25%" bgcolor="#a9d08e">
-                                            <div align="center"><strong>MOTIVO</strong></div>
-                                        </td>
-
-                                    </tr>
-                                    @if (sizeof($reportes) == 0)
-                                        <tr>
-                                            <td colspan="12">
-                                                <div align="center">NO HAY ACREDITACIONES EN ESTA SEMANA</div>
-                                            </td>
-                                        </tr>
-                                    @else
-                                        @foreach ($reportes as $dato)
-                                        @php
-                                            $item ++;
-                                        @endphp
-                                            <tr>
-                                                <td style="font-size:10px">
-                                                    <div align="center">{{ $item }}</div>
-                                                </td>
-                                                <td style="font-size:10px">
-                                                    <div>{{ $dato['empleado_info'] }}</div>
-                                                </td>
-                                                <td style="font-size:10px">
-                                                    <div>{{ $dato['monto_generado'] }}</div>
-                                                </td>
-                                                <td style="font-size:10px">
-                                                    <div>{{ $dato['monto_modificado'] }}</div>
-                                                </td>
-                                                <td style="font-size:10px">
-                                                    <div>{{ $dato['motivo'] }}</div>
-                                                </td>
-
-                                            </tr>
-                                        @endforeach
-                                        <tr>
-                                            <td colspan="3" style="font-size:10px"> <div align="center"><strong>SUMA</strong></div></td>
-                                            <td style="font-size:14px">
-                                                {{ $suma }}
-                                            </td>
-                                            <td></td>
-                                        </tr>
-                                    @endif
-
-                                </table>
+                            <td width="8%" bgcolor="#a9d08e">
+                                <div align="center"><strong>ITEM</strong></div>
                             </td>
+                            <td width="50%" bgcolor="#a9d08e">
+                                <div align="center"><strong>EMPLEADO</strong></div>
+                            </td>
+                            <td width="15%" bgcolor="#a9d08e">
+                                <div align="center"><strong>SALDO ACTUAL</strong></div>
+                            </td>
+                            <td width="17%" bgcolor="#a9d08e">
+                                <div align="center"><strong>MONTO ASIGNADO</strong></div>
+                            </td>
+                            <td width="25%" bgcolor="#a9d08e">
+                                <div align="center"><strong>MOTIVO</strong></div>
+                            </td>
+
                         </tr>
+                        @if (sizeof($reportes) == 0)
+                            <tr>
+                                <td colspan="12">
+                                    <div align="center">NO HAY ACREDITACIONES EN ESTA SEMANA</div>
+                                </td>
+                            </tr>
+                        @else
+                            @foreach ($reportes as $dato)
+                            @php
+                                $item ++;
+                            @endphp
+                                <tr>
+                                    <td style="font-size:10px">
+                                        <div align="center">{{ $item }}</div>
+                                    </td>
+                                    <td style="font-size:10px">
+                                        <div>{{ $dato['empleado_info'] }}</div>
+                                    </td>
+                                    <td style="font-size:10px">
+                                        <div align="right">{{ $dato['saldo_actual'] }}</div>
+                                    </td>
+                                    <td style="font-size:10px">
+                                        <div align="right">{{ $dato['monto_modificado'] }}</div>
+                                    </td>
+                                    <td style="font-size:10px">
+                                        <div>{{ $dato['motivo'] }}</div>
+                                    </td>
+
+                                </tr>
+                            @endforeach
+                            <tr>
+                                <td colspan="3" style="font-size:10px"> <div align="center"><strong>TOTAL DE MONTO ASIGNADO</strong></div></td>
+                                <td style="font-size:14px">
+                                   {{ $suma }}
+                                </td>
+                                <td></td>
+                            </tr>
+                        @endif
                     </table>
                 </div>
             </td>
