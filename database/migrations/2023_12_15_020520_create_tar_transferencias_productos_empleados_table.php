ba03c8c27997d97779726a968c0686583ce7fa4d
juancuestav <juanbryanc@gmail.com>
2023-12-17 23:51:51 -0500

    Tareas: Avance en transferencia de productos entre empleados

---
+++ database/migrations/2023_12_15_020520_create_tar_transferencias_productos_empleados_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Models\Autorizacion;
+use App\Models\Tareas\TransferenciaProductoEmpleado;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tar_transferencias_productos_empleados', function (Blueprint $table) {
+            $table->id();
+            $table->text('justificacion');
+            $table->text('causa_anulacion')->nullable();
+            $table->enum('estado', [TransferenciaProductoEmpleado::PENDIENTE, TransferenciaProductoEmpleado::COMPLETA, TransferenciaProductoEmpleado::ANULADA])->default(TransferenciaProductoEmpleado::PENDIENTE);
+            $table->text('observacion_aut')->nullable()->after('solicitante_id');
+            $table->unsignedBigInteger('solicitante_id');
+            $table->unsignedBigInteger('empleado_origen_id');
+            $table->unsignedBigInteger('empleado_destino_id');
+            $table->unsignedBigInteger('tarea_origen_id');
+            $table->unsignedBigInteger('tarea_destino_id');
+            $table->unsignedBigInteger('autorizacion_id')->default(Autorizacion::PENDIENTE);
+            $table->unsignedBigInteger('autorizador_id');
+
+            $table->foreign('solicitante_id')->references('id')->on('empleados')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('empleado_origen_id')->references('id')->on('empleados')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('empleado_destino_id')->references('id')->on('empleados')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('tarea_origen_id')->references('id')->on('tareas')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('tarea_destino_id')->references('id')->on('tareas')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('autorizador_id')->references('id')->on('empleados')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('autorizacion_id')->references('id')->on('autorizaciones')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tar_transferencias_productos_empleados');
+    }
+};
--- routes/api_tareas.php
+++ routes/api_tareas.php
@@ -26,6 +26,7 @@ use App\Http\Controllers\TendidoController;
 use App\Http\Controllers\TareaController;
 use App\Http\Controllers\Tareas\EtapaController;
 use App\Http\Controllers\Tareas\TransferenciaMaterialEmpleadoController;
+use App\Http\Controllers\Tareas\TransferenciaProductoEmpleadoController;
 use Illuminate\Support\Facades\Route;
 
 // Generar GET - POST - PUT - DELETE
@@ -50,7 +51,7 @@ Route::apiResources(
         'seguimientos' => SeguimientoSubtareaController::class,
         'archivos-seguimientos' => ArchivoSeguimientoController::class,
         'actividades-realizadas-seguimientos-subtareas' => ActividadRealizadaSeguimientoSubtareaController::class,
-        'transferencias-materiales-empleados' => TransferenciaMaterialEmpleadoController::class,
+        'transferencias-productos-empleados' => TransferenciaProductoEmpleadoController::class,
     ],
     [
         'parameters' => [
@@ -65,7 +66,7 @@ Route::apiResources(
             'rutas-tareas' => 'ruta_tarea',
             'archivos-seguimientos' => 'archivo_seguimiento',
             'actividades-realizadas-seguimientos-subtareas' => 'actividad_realizada',
-            'transferencias-materiales-empleados' => 'transferencia_material_empleado'
+            'transferencias-productos-empleados' => 'transferencia_producto_empleado'
         ],
     ]
 );
