a368eb3733d4135d2ecfff6aa05fc8de08c232e4
juancuestav <juanbryanc@gmail.com>
2024-01-12 08:15:15 -0500

    Tareas correcion

--- app/Http/Controllers/TareaController.php
+++ app/Http/Controllers/TareaController.php
@@ -53,10 +53,10 @@ class TareaController extends Controller
         } */
 
         if (request('formulario')) {
-            /* return Tarea::ignoreRequest(['campos', 'formulario'])->filter()->where('finalizado', false)->orWhere(function ($query) {
+            return Tarea::ignoreRequest(['campos', 'formulario'])->filter()->where('finalizado', false)->orWhere(function ($query) {
                 $query->where('finalizado', true)->disponibleUnaHoraFinalizar();
-            })->latest()->get(); */
-            return $this->tareaService->obtenerTareasAsignadasEmpleadoLuegoFinalizar(request('empleado_id'));
+            })->latest()->get();
+            // return $this->tareaService->obtenerTareasAsignadasEmpleadoLuegoFinalizar(request('empleado_id'));
         }
 
         if (request('activas_empleado')) return $this->tareaService->obtenerTareasAsignadasEmpleado(request('empleado_id'));
