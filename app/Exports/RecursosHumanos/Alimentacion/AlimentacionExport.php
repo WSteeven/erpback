cbd1c021c20a512f7cf2234d09c00dd2a92d3d65
henvisi-1994 <henvisi_1994@hotmail.com>
2024-01-15 19:49:34 -0500

    feat:reporte de alimentacion: reporte de alimentacion en excel

--- app/Exports/RecursosHumanos/Alimentacion/AlimentacionExport.php
+++ app/Exports/RecursosHumanos/Alimentacion/AlimentacionExport.php
@@ -2,15 +2,31 @@
 
 namespace App\Exports\RecursosHumanos\Alimentacion;
 
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
 
-class AlimentacionExport implements FromCollection
+class AlimentacionExport implements FromView,ShouldAutoSize,WithColumnWidths
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
+    protected $reporte;
+
+    function __construct($reporte)
     {
-        //
+        $this->reporte = $reporte;
+    }
+    public function view(): View
+    {
+        return view('recursos-humanos.excel.reporte_alimentacion',$this->reporte);
+    }
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 5,
+            'B' => 41,
+            'C' => 20,
+            'D' => 20,
+            'E' => 45
+        ];
     }
 }
