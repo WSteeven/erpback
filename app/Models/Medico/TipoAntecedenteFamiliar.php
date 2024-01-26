8c7a287b8f3eb991e2c39f31d6718ffdb19c1821
henvisi-1994 <henvisi_1994@hotmail.com>
2024-01-26 10:38:06 -0500

    feat:antecedentes personales

---
+++ app/Models/Medico/TipoAntecedenteFamiliar.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Medico;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TipoAntecedenteFamiliar extends Model
+{
+    use HasFactory;
+}
