d14565ae87f7fab594db504bd99a9a4a1a556590
henvisi-1994 <henvisi_1994@hotmail.com>
2024-01-30 17:02:26 -0500

    feat:rutas modulo medico

--- routes/api_medicos.php
+++ routes/api_medicos.php
@@ -9,52 +9,87 @@ use App\Http\Controllers\Medico\AptitudMedicaController;
 use App\Http\Controllers\Medico\CategoriaExamenController;
 use App\Http\Controllers\Medico\ConfiguracionExamenCampoController;
 use App\Http\Controllers\Medico\ConfiguracionExamenCategoriaController;
+use App\Http\Controllers\Medico\ConstanteVitalController;
+use App\Http\Controllers\Medico\DescripcionAntecedenteTrabajoController;
 use App\Http\Controllers\Medico\DetalleExamenController;
 use App\Http\Controllers\Medico\DetalleResultadoExamenController;
+use App\Http\Controllers\Medico\DiagnosticoController;
 use App\Http\Controllers\Medico\EsquemaVacunaController;
 use App\Http\Controllers\Medico\EstadoExamenController;
 use App\Http\Controllers\Medico\EstadoSolicitudExamenController;
+use App\Http\Controllers\Medico\EstiloVidaController;
 use App\Http\Controllers\Medico\ExamenController;
+use App\Http\Controllers\Medico\ExamenEspecificoController;
+use App\Http\Controllers\Medico\ExamenExamenPreocupacionalController;
+use App\Http\Controllers\Medico\ExamenFisicoRegionalController;
+use App\Http\Controllers\Medico\FactorRiesgoController;
+use App\Http\Controllers\Medico\HabitoToxicoController;
 use App\Http\Controllers\Medico\IdentidadGeneroController;
+use App\Http\Controllers\Medico\MedicacionController;
 use App\Http\Controllers\Medico\OrientacionSexualController;
 use App\Http\Controllers\Medico\PreocupacionalController;
 use App\Http\Controllers\Medico\RegistroEmpleadoExamenController;
 use App\Http\Controllers\Medico\ReligionController;
 use App\Http\Controllers\Medico\ResultadoExamenController;
+use App\Http\Controllers\Medico\RevisionActualOrganoSistemaController;
+use App\Http\Controllers\Medico\SistemaOrganicoController;
 use App\Http\Controllers\Medico\TipoAntecedenteController;
 use App\Http\Controllers\Medico\TipoAntecedenteFamiliarController;
+use App\Http\Controllers\Medico\TipoAptitudController;
 use App\Http\Controllers\Medico\TipoExamenController;
+use App\Http\Controllers\Medico\TipoFactorRiesgoController;
+use App\Http\Controllers\Medico\TipoHabitoToxicoController;
 use App\Http\Controllers\Medico\TipoVacunaController;
+use App\Models\Medico\CategoriaExamenFisico;
+use App\Models\Medico\CategoriaFactorRiesgo;
 use Illuminate\Support\Facades\Route;
 
 // Generar GET - POST - PUT - DELETE
 Route::apiResources(
     [
-        'tipos-examenes' => TipoExamenController::class,
-        'examenes' => ExamenController::class,
-        'categorias-examenes' => CategoriaExamenController::class,
-        'estados-examenes' => EstadoExamenController::class,
-        'detalles-examenes' => DetalleExamenController::class,
-        'configuraciones-examenes-categ' => ConfiguracionExamenCategoriaController::class,
-        'registros-empleados-examenes' => RegistroEmpleadoExamenController::class,
-        'estados-solicitudes-examenes' => EstadoSolicitudExamenController::class,
-        'configuraciones-examenes-campos' => ConfiguracionExamenCampoController::class,
-        'resultados-examenes-campos' => ResultadoExamenController::class,
-        'detalles-resultados-examenes' => DetalleResultadoExamenController::class,
-        'tipos-vacunas' => TipoVacunaController::class,
-        'esquemas-vacunas' => EsquemaVacunaController::class,
-        'religiones' => ReligionController::class,
-        'orientaciones-sexuales' => OrientacionSexualController::class,
-        'identidades-generos' => IdentidadGeneroController::class,
-        'tipos-antecedentes' => TipoAntecedenteController::class,
         'actividades-puesto-trabajo', ActividadPuestoTrabajoController::class,
         'antecedentes-familiares', TipoAntecedenteFamiliarController::class,
         'antecedentes-gineco-obstetricos', AntecedenteGinecoObstetricoController::class,
         'antecedentes-personales', AntecedentePersonalController::class,
         'antecedentes-trabajos-anteriores', AntecedenteTrabajoAnteriorController::class,
         'aptitudes-medicas', AptitudMedicaController::class,
-        'categorias-examenes', CategoriaExamenController::class,
+        'categorias-examenes' => CategoriaExamenController::class,
+        'categorias-examenes-fisicos' => CategoriaExamenFisico::class,
+        'categorias-factores-riesgos' => CategoriaFactorRiesgo::class,
+        'configuraciones-examenes-campos' => ConfiguracionExamenCampoController::class,
+        'configuraciones-examenes-categoria' => ConfiguracionExamenCategoriaController::class,
+        'constantes-vitales' => ConstanteVitalController::class,
+        'descripciones-antecedentes-examenes' => DescripcionAntecedenteTrabajoController::class,
+        'detalles-examenes' => DetalleExamenController::class,
+        'detalles-resultados-examenes' => DetalleResultadoExamenController::class,
+        'diagnosticos' => DiagnosticoController::class,
+        'esquemas-vacunas' => EsquemaVacunaController::class,
+        'estados-examenes' => EstadoExamenController::class,
+        'estados-solicitudes-examenes' => EstadoSolicitudExamenController::class,
+        'estilos-vida' => EstiloVidaController::class,
+        'examenes' => ExamenController::class,
+        'examenes-especificos' => ExamenEspecificoController::class,
+        'examenes-fisicos-regionales' => ExamenFisicoRegionalController::class,
+        'examenes-preocupacionales' => ExamenExamenPreocupacionalController::class,
+        'factores-riesgo' => FactorRiesgoController::class,
+        'habitos-toxicos' => HabitoToxicoController::class,
+        'identidades-generos' => IdentidadGeneroController::class,
+        'medicaciones' => MedicacionController::class,
+        'orientaciones-sexuales' => OrientacionSexualController::class,
         'preocupacionales', PreocupacionalController::class,
+        'registros-empleados-examenes' => RegistroEmpleadoExamenController::class,
+        'religiones' => ReligionController::class,
+        'resultados-examenes' => ResultadoExamenController::class,
+        'revisiones-actuales-organos' => RevisionActualOrganoSistemaController::class,
+        'sistemas-organicos' => SistemaOrganicoController::class,
+        'tipos-antecedentes' => TipoAntecedenteController::class,
+        'tipos-antecedentes-familiares' => TipoAntecedenteFamiliarController::class,
+        'tipos-aptitudes' => TipoAptitudController::class,
+        'tipos-examenes' => TipoExamenController::class,
+        'tipos-examenes' => TipoExamenController::class,
+        'tipos-factores-riesgos' => TipoFactorRiesgoController::class,
+        'tipos-habitos-toxicos' => TipoHabitoToxicoController::class,
+        'tipos-vacunas' => TipoVacunaController::class,
     ],
     [
         'parameters' => [
