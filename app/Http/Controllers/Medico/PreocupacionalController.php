3785ac13c4fb45a96ae7a18872c8bf410bfc3e87
henvisi-1994 <henvisi_1994@hotmail.com>
2024-01-30 08:20:28 -0500

    feat: insercion de datos

--- app/Http/Controllers/Medico/PreocupacionalController.php
+++ app/Http/Controllers/Medico/PreocupacionalController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers\Medico;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Medico\PreocupacionalRequest;
 use App\Http\Resources\Medico\PreocupacionalResource;
+use App\Models\Empleado;
+use App\Models\Medico\AntecedenteGinecoObstetrico;
+use App\Models\Medico\AntecedentePersonal;
+use App\Models\Medico\ConstanteVital;
+use App\Models\Medico\DescripcionAntecedenteTrabajo;
 use App\Models\Medico\Preocupacional;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
+use Src\App\Medico\PreocupacionalService;
 use Src\Shared\Utils;
 
 class PreocupacionalController extends Controller
@@ -37,6 +43,49 @@ class PreocupacionalController extends Controller
             $datos = $request->validated();
             DB::beginTransaction();
             $preocupacional = Preocupacional::create($datos);
+            $preocupacional_service = new PreocupacionalService($preocupacional->id);
+            $preocupacional_service->insertarAntecedentesPersonales(new AntecedentePersonal([
+                'antecedentes_quirorgicos' => $request->antecedentes_quirorgicos,
+                'vida_sexual_activa' => $request->vida_sexual_activa,
+                'tiene_metodo_planificacion_familiar' => $request->tiene_metodo_planificacion_familiar,
+                'tipo_metodo_planificacion_familiar' => $request->tipo_metodo_planificacion_familiar,
+            ]));
+            $empleado = Empleado::find($preocupacional->empleado_id);
+            $genero = $empleado?->genero;
+            if($genero === 'F'){
+                $preocupacional_service->insertarAntecedentesGinecoObstetricos(new AntecedenteGinecoObstetrico([
+                    'menarquia' => $request->menarquia,
+                    'ciclos' => $request->ciclos,
+                    'fecha_ultima_menstruacion' => $request->fecha_ultima_menstruacion,
+                    'gestas' => $request->gestas,
+                    'partos' => $request->partos,
+                    'cesareas' => $request->cesareas,
+                    'abortos' => $request->abortos,
+                    'hijos_vivos' => $request->hijos_vivos,
+                    'hijos_muertos' => $request->hijos_muertos,
+                ]));
+            }
+                $preocupacional_service->insertarDescripcionAntecedenteTrabajo(
+                    new DescripcionAntecedenteTrabajo([
+                        'calificado_iess' => $request->calificado_iess,
+                        'descripcion' => $request->descripcion,
+                        'fecha' => $request->fecha,
+                        'observacion' => $request->observacion,
+                        'tipo_descripcion_antecedente_trabajo' => $request->tipo_descripcion_antecedente_trabajo,
+                    ])
+                );
+                $preocupacional_service->insertarConstanteVital(new ConstanteVital([
+                    'presion_arterial' => $request->presion_arterial,
+                    'temperatura' => $request->temperatura,
+                    'frecuencia_cardiaca' => $request->frecuencia_cardiaca,
+                    'saturacion_oxigeno' => $request->saturacion_oxigeno,
+                    'frecuencia_respiratoria' => $request->frecuencia_respiratoria,
+                    'peso' => $request->peso,
+                    'estatura' => $request->estatura,
+                    'talla' => $request->talla,
+                    'indice_masa_corporal' => $request->indice_masa_corporal,
+                    'perimetro_abdominal' => $request->perimetro_abdominal,
+                ]));
             $modelo = new PreocupacionalResource($preocupacional);
             $this->tabla_roles($preocupacional);
             $mensaje = Utils::obtenerMensaje($this->entidad, 'store');
