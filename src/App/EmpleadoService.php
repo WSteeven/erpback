587e1dab0e5f018dc9340c57bc75342bf0275d98
antonnick28 <henvisi_1994@hotmail.com>
2024-03-13 15:33:28 -0500

    Se agrega el campo 'Fecha de Nacimiento' en la taba de reporte de Empleados de RRHH

--- src/App/EmpleadoService.php
+++ src/App/EmpleadoService.php
@@ -65,6 +65,7 @@ class EmpleadoService
                 'jefe_id',
                 'canton_id',
                 'direccion',
+                'fecha_nacimiento',
                 'estado',
                 'grupo_id',
                 'cargo_id',
