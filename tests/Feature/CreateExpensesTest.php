86fc09ae047244f4b0190d2ef6a7b1933f5cfb1c
henvisi-1994 <henvisi_1994@hotmail.com>
2024-03-25 17:02:40 -0500

    feat: testing

--- tests/Feature/CreateExpensesTest.php
+++ tests/Feature/CreateExpensesTest.php
@@ -6,16 +6,19 @@ use App\Models\Canton;
 use App\Models\Empleado;
 use App\Models\FondosRotativos\Gasto\DetalleViatico;
 use App\Models\FondosRotativos\Gasto\Gasto;
+use App\Models\FondosRotativos\Gasto\SubDetalleViatico;
 use App\Models\Proyecto;
 use App\Models\Subtarea;
 use App\Models\Tarea;
 use Faker\Factory;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
 class CreateExpensesTest extends TestCase
 {
-    private $cantidad=0;
-    private  $valor_u =0;
+    private $cantidad = 0;
+    private  $valor_u = 0;
     /**
      * A basic feature test example.
      *
@@ -26,34 +29,55 @@ class CreateExpensesTest extends TestCase
         $faker = Factory::create('es_PE');
         $this->setCantidad($faker->numberBetween(1, 13));
         $this->setValorU($faker->numberBetween(1, 13));
-      //Arrange:
-      $expenseData =[
-        'fecha_viat' => $faker->dateTimeBetween($startDate = '-1 month', $endDate = 'now')->format('Y-m-d'),
-        'id_lugar' => Canton::inRandomOrder()->value('id') ,
-        'id_tarea' => Tarea::inRandomOrder()->value('id') ,
-        'id_proyecto' => Proyecto::inRandomOrder()->value('id'),
-        'ruc' => $this->randomRUC(),
-        'factura' => $this->randomFactura(),
-        'aut_especial' =>$faker->numberBetween(28, 30),
-        'detalle' =>  DetalleViatico::inRandomOrder()->value('id'),
-        'cantidad' => $this->cantidad,
-        'valor_u' => $this->valor_u,
-        'total' => $this->cantidad * $this->valor_u,
-        'comprobante' => $faker->imageUrl(150,150),
-        'comprobante2' =>  $faker->imageUrl(150,150),
-        'observacion' =>$faker->text(200),
-        'id_usuario' =>Empleado::inRandomOrder()->value('id'),
-        'estado' => Gasto::PENDIENTE,
-        'detalle_estado' => null
-      ];
+        //Arrange:
+        $expenseData = [
+            'fecha_viat' => $faker->dateTimeBetween($startDate = '-1 month', $endDate = 'now')->format('Y-m-d'),
+            'lugar' => Canton::inRandomOrder()->value('id'),
+            'num_tarea' => Tarea::inRandomOrder()->value('id'),
+            'proyecto' => Proyecto::inRandomOrder()->value('id'),
+            'ruc' => '0704265099001',
+            'factura' => $this->randomFactura(),
+            'aut_especial' => $faker->numberBetween(28, 30),
+            'detalle' =>  DetalleViatico::inRandomOrder()->value('id'),
+            'sub_detalle' => $this->generarArraySubDetalle(),
+            'cantidad' => $this->cantidad,
+            'valor_u' => $this->valor_u,
+            'total' => $this->cantidad * $this->valor_u,
+            'comprobante' => $faker->imageUrl(150, 150),
+            'comprobante2' =>  $faker->imageUrl(150, 150),
+            'observacion' => $faker->text(200),
+            'id_usuario' => Empleado::inRandomOrder()->value('id'),
+            'estado' => Gasto::PENDIENTE,
+            'detalle_estado' => null
+        ];
+        Log::channel('testing')->info('Log', ['expenseData', $expenseData]);
 
+       $usuario = [
+            "name" => "hsimbana",
+            "password" => "0704265099"
+        ];
+        $response = Http::post('http://localhost:8000/api/usuarios/login', $usuario);
 
-      //Act
-      $response =$this->post('api/fondos-rotativos/gastos', $expenseData);
+        $headers = [
+            'Authorization' => 'Bearer ' . $response->json()['access_token']
+        ];
 
-      //Assert
-      $response->assertStatus(500);
-      $this->assertDatabaseHas('gastos', $expenseData);
+        //Act
+      //  $response = $this->post('api/fondos-rotativos/gastos', $expenseData);
+        $response = $this->post('api/fondos-rotativos/gastos', $expenseData, $headers);
+
+        //Assert
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('gastos', $expenseData);
+    }
+
+    private function generarArraySubDetalle()
+    {
+        $array = array();
+        for ($x = 1; $x <= 3; $x++) {
+            $array[] = SubDetalleViatico::inRandomOrder()->value('id') ;
+        }
+        return $array;
     }
     private function randomRUC()
     {
