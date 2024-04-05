3a69afe0fb6bf4d476d2520b74c2a2875124d5c4
henvisi-1994 <henvisi_1994@hotmail.com>
2024-04-05 12:11:26 -0500

    fix:diseño y cambio de variables en reportes de modulo de fondos rotativos

--- app/Exports/TranferenciaSaldoExport.php
+++ app/Exports/TranferenciaSaldoExport.php
@@ -4,14 +4,28 @@ namespace App\Exports;
 
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
 
-class TranferenciaSaldoExport implements FromView
+class TranferenciaSaldoExport implements FromView,ShouldAutoSize,WithColumnWidths
 {
     protected $reporte;
 
     function __construct($reporte)
     {
         $this->reporte = $reporte;
+    }
+    public function columnWidths(): array
+    {
+        return [
+            'A'=>10,
+            'B'=>36,
+            'C'=>36,
+            'D'=>8,
+            'E'=>15,
+            'F'=>27,
+            'G'=>100,
+        ];
     }
     public function view(): View
     {
