16b994159281338f3e9e35bb72755f761bd8bcb7
henvisi-1994 <henvisi_1994@hotmail.com>
2024-05-14 17:03:44 -0500

    feta:almacenar usuario

--- app/Http/Controllers/LoginSocialNetworkController.php
+++ app/Http/Controllers/LoginSocialNetworkController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Laravel\Socialite\Facades\Socialite;
 
@@ -15,7 +17,14 @@ class LoginSocialNetworkController extends Controller
     }
     public function handleCallback($driver, Request $request)
     {
-        $user = Socialite::driver($driver)->stateless()->user();
-        return response()->json(['postulante' => $user], 200);
+        $user_social = Socialite::driver($driver)->stateless()->user();
+        $user = User::updateOrCreate([
+            'github_id' =>  $user_social->id,
+        ], [
+            'name' =>  $user_social->name,
+            'email' =>  $user_social->email,
+        ]);
+        Auth::login($user);
+        return redirect('/');
     }
 }
