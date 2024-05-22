077aeecf5446009d50425f4c39d13d9ff82baa13
WSteeven <wilson972906@gmail.com>
2024-05-22 17:04:42 -0500

    avances

--- app/Jobs/Vehiculos/ActualizarMantenimientoVehiculoJob.php
+++ app/Jobs/Vehiculos/ActualizarMantenimientoVehiculoJob.php
@@ -39,8 +39,19 @@ class ActualizarMantenimientoVehiculoJob implements ShouldQueue
             //Buscamos el plan de mantenimiento y la ultima bitacora del vehiculo para comparar los kms transcurridos
             $plan = PlanMantenimiento::where('vehiculo_id', $mantenimiento->vehiculo_id)->where('servicio_id', $mantenimiento->servicio_id)->get();
             if ($plan) {
-                $bitacora = BitacoraVehicular::where('vehiculo_id', $mantenimiento->vehiculo_id)->where('firmada', true)->orderBy('id', 'desc')->get();
-                
+                $bitacora = BitacoraVehicular::where('vehiculo_id', $mantenimiento->vehiculo_id)->where('firmada', true)->orderBy('id', 'desc')->first();
+                $ultimoMantenimiento = MantenimientoVehiculo::where('vehiculo_id', $mantenimiento->vehiculo_id)->where('servicio_id', $mantenimiento->servicio_id)->where('estado', MantenimientoVehiculo::REALIZADO)->orderBy('id', 'desc')->first();
+                if ($ultimoMantenimiento) {
+                    //Se realiza el calculo en base al km_realizado del ultimo mantenimiento
+                    if ($bitacora->km_final > $ultimoMantenimiento->km_realizado + $plan->aplicar_cada) {
+                        $mantenimiento->estado = MantenimientoVehiculo::RETRASADO;
+                        $mantenimiento->km_retraso = $bitacora->km_final - ($ultimoMantenimiento->km_realizado + $plan->aplicar_cada);
+                        $mantenimiento->save();
+                    }
+                } else {
+                    //Se realiza el calculo como que fuera el primer mantenimiento
+
+                }
             }
         }
     }
