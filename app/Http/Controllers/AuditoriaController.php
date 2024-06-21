a541da6295cd0ab47488b1d6b05e6dfa991d4aad
WSteeven <wilson972906@gmail.com>
2024-06-21 08:58:20 -0500

    avances en auditorias para obtener por varios filtros

--- app/Http/Controllers/AuditoriaController.php
+++ app/Http/Controllers/AuditoriaController.php
@@ -12,8 +12,16 @@ use OwenIt\Auditing\Models\Audit;
 
 class AuditoriaController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('can:puede.acceder.auditorias')->only('index');
+    }
+
+
+
     public function index(Request $request)
     {
+        // Log::channel('testing')->info('Log', ['Request', $request->all()]);
         $empleado = Empleado::find($request->empleado);
 
         //formato para obtener las fechas al inicio del día y al final del día
@@ -21,14 +29,26 @@ class AuditoriaController extends Controller
         $fecha_fin = $request->fecha_fin ? Carbon::parse($request->fecha_fin)->endOfDay() : Carbon::now();
 
 
-        $results = Audit::where('user_id', $empleado->usuario_id)->whereBetween('created_at', [$fecha_inicio, $fecha_fin])->orderBy('updated_at', 'desc')->get();
+        $results = Audit::when($request->empleado_id, function ($q) use ($empleado) {
+            $q->where('user_id', $empleado->usuario_id);
+        })
+            ->when($request->auditable_id, function ($q) use ($request) {
+                $q->where('auditable_id', $request->auditable_id);
+            })
+            ->when($request->auditable_type, function ($q) use ($request) {
+                $q->where('auditable_type', 'like', '%' . $request->auditable_type . '%');
+            })
+            ->when($request->fecha_inicio, function ($q) use ($fecha_inicio, $fecha_fin) {
+                $q->whereBetween('created_at', [$fecha_inicio, $fecha_fin]);
+            })
+            ->orderBy('updated_at', 'desc')->get();
         $modelosAfectados = $results->unique('auditable_type')->pluck('auditable_type')->toArray();
         $modelos = array_map(function ($model) {
             return class_basename($model);
         }, $modelosAfectados);
 
-        Log::channel('testing')->info('Log', ['Empleado audits', $results]);
-        Log::channel('testing')->info('Log', ['Modelos', $modelos]);
+        Log::channel('testing')->info('Log', ['Empleado audits', $results->count()]);
+        // Log::channel('testing')->info('Log', ['Modelos', $modelos]);
 
         $results = AuditResource::collection($results);
 
@@ -40,11 +60,11 @@ class AuditoriaController extends Controller
     {
         $results = [];
         $modelos = Audit::groupBy('auditable_type')->pluck('auditable_type')->toArray();
-        $modelos = array_map(function ($model) {
+        $results = array_map(function ($model) {
             return class_basename($model);
         }, $modelos);
 
-        Log::channel('testing')->info('Log', ['metodo modelos', $modelos]);
+        Log::channel('testing')->info('Log', ['metodo modelos', $results]);
         return response()->json(compact('results'));
     }
 }
