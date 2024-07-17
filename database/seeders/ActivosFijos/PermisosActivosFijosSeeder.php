d479ce7596eb2051a5bfd74f33ecd4e9d18601eb
juancuestav <juanbryanc@gmail.com>
2024-07-17 16:50:34 -0500

    Merge desarrollo / jpcustody

---
+++ database/seeders/ActivosFijos/PermisosActivosFijosSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders\ActivosFijos;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Src\Config\Permisos;
+
+class PermisosActivosFijosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * php artisan db:seed --class=Database\Seeders\ActivosFijos\PermisosActivosFijosSeeder
+     * @return void
+     */
+    public function run()
+    {
+        /***************************
+         * Modulo de Activos Fijos
+         ***************************/
+        $activos_fijos = Role::firstOrCreate(['name' => User::ROL_ACTIVOS_FIJOS]);
+
+        // Modulo activos fijos
+        Permission::firstOrCreate(['name' => Permisos::ACCEDER . 'modulo_activos_fijos'])->syncRoles([$activos_fijos]);
+
+        // Control de activos fijos
+        Permission::firstOrCreate(['name' => Permisos::VER . 'control_activos_fijos'])->syncRoles([$activos_fijos]);
+        Permission::firstOrCreate(['name' => Permisos::ACCEDER . 'control_activos_fijos'])->syncRoles([$activos_fijos]);
+    }
+}
