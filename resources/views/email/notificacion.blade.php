321a7e762f4d9127b5adfbb8f90efc4f99940b74
WSteeven <wilson972906@gmail.com>
2025-03-05 15:24:06 -0500

    fix: correcciones en recuperar cuenta y codigo de verificacion

--- resources/views/email/notificacion.blade.php
+++ resources/views/email/notificacion.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     @php
-        $logo_principal = 'data:image/png;base64,' . base64_encode(file_get_contents(public_path() . $configuracion['logo_claro']));
+        use Src\Shared\Utils;
     @endphp
 <head>
     <meta charset="UTF-8">
@@ -10,6 +10,6 @@
 </head>
 <body>
     <h3>Este es un mensaje de prueba</h3>
-    <img src="{{ $logo_principal }}" alt="logo" width="100" height="100"/>
+    <img src="{{ Utils::urlToBase64(url($configuracion->logo_claro)) }}" alt="logo" width="100" height="100"/>
 </body>
 </html>
--- src/App/ComprasProveedores/ProformaService.php
+++ src/App/ComprasProveedores/ProformaService.php
@@ -63,7 +63,7 @@ class ProformaService
 
     public static function filtrarProformasJefeTecnico($request)
     {
-        $ids_buscar = [3, 27];//3=Jonathan Veintimilla, 27=Joao Celi
+        $ids_buscar = [3, 27, 30];//3=Jonathan Veintimilla, 27=Joao Celi, 30 Nicolas Pazmiño
         $query = Proforma::where(function ($q) use ($ids_buscar) {
             $q->orWhereIn('solicitante_id', $ids_buscar)
                 ->orWhereIn('autorizador_id', $ids_buscar);
